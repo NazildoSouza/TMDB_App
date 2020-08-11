573b6cc84bd25f1ac44aa5cb8b1cdafa2bb1a8df
Nazildo Souza <nsouza10@outlook.com>
2020-08-10 22:42:53 -0300

    Initial Commit

---
+++ TMDB_App/TMDB_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_AppApp.swift
+//  TMDB_App
+//
+//  Created by Nazildo Souza on 10/08/20.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
